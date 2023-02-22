f6652d47f9d6dd786ae6da47e41ff1a3d129d139
aaklabs <connectakonline@gmail.com>
2023-02-21 20:03:19 -0500

    compare logic enhanced with error/exception

--- datadiff-be/src/main/java/com/aak/util/compare/diff/CompareManager.java
+++ datadiff-be/src/main/java/com/aak/util/compare/diff/CompareManager.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.aak.util.compare.model.DBColumn;
 import com.aak.util.compare.model.DBRow;
 import com.aak.util.compare.model.api.ColumnCompareResult;
+import com.aak.util.compare.model.api.CompareResponse;
 import com.aak.util.compare.model.api.RowCompareResult;
 
 import lombok.extern.slf4j.Slf4j;
@@ -91,11 +92,12 @@ public class CompareManager {
 			compareResultItem.setError(true);
 		}
 		
-		
-	        //StringUtils.join(row) + (unmatched ? " - UNMATCHED" : " - MATCHED");
+		//StringUtils.join(row) + (unmatched ? " - UNMATCHED" : " - MATCHED");
 		return compareResultItem;
 	}
 
+	//db1 has more rows than db2
+	//db1 has more col than db2
 	public List<RowCompareResult> compareItem(List<DBRow> db1Rows, List<DBRow> db2Rows) {
 		int db1RowCount = db1Rows.size();
 		int db2RowCount = db2Rows.size();
@@ -110,34 +112,44 @@ public class CompareManager {
 		List<RowCompareResult> compareResult = new ArrayList<>();
 		
 		for(int i=0; i < db1RowCount; i++) {
-			DBRow db1RowRecord = db1Rows.get(i);
-			DBRow db2RowRecord = db2Rows.get(i);
-			
-			
-			int db1RowItemColumnCount = db1RowRecord.getColumns().size();
-			int db2RowItemColumnCount = db2RowRecord.getColumns().size();
-			
-			if(db1RowItemColumnCount != db2RowItemColumnCount) {
-				log.error("Column count not maching for db1RowRecord ->{} and db2RowRecord ->{}",db1RowItemColumnCount,db2RowItemColumnCount);
-			}
-			if(db1RowItemColumnCount ==0 || db2RowItemColumnCount == 0) {
-				log.error("Column count zero for db1RowRecord ->{} or db2RowRecord ->{}",db1RowItemColumnCount,db2RowItemColumnCount);
-			}
-			
-			
-			RowCompareResult rowCompareResult = new RowCompareResult();
-			log.info("db1RowItemColumnCount ->{} db2RowItemColumnCount->{}",db1RowItemColumnCount,db2RowItemColumnCount );
-			
-			
-			for (int j = 0; j < db1RowItemColumnCount; j++) {
+			DBRow db1RowRecord = null;
+			DBRow db2RowRecord = null;
+			try {
+				db1RowRecord = db1Rows.get(i);
+				db2RowRecord = db2Rows.get(i);
 				
-				DBColumn db1RowColRecord = db1RowRecord.getColumns().get(j);
-				DBColumn db2RowColRecord = db2RowRecord.getColumns().get(j);
-				ColumnCompareResult compareResultItem = compareItem(db1RowColRecord, db2RowColRecord);
-				rowCompareResult.getColumns().add(compareResultItem);
+				int db1RowItemColumnCount = db1RowRecord.getColumns().size();
+				int db2RowItemColumnCount = db2RowRecord.getColumns().size();
+				
+				if(db1RowItemColumnCount != db2RowItemColumnCount) {
+					String msg ="Column count not maching for db1RowRecord ->"+db1RowItemColumnCount+" and db2RowRecord ->"+db2RowItemColumnCount; 
+					log.error(msg);
+				}
+				if(db1RowItemColumnCount ==0 || db2RowItemColumnCount == 0) {
+					log.error("Column count zero for db1RowRecord ->{} or db2RowRecord ->{}",db1RowItemColumnCount,db2RowItemColumnCount);
+				}
+				
+				RowCompareResult rowCompareResult = new RowCompareResult();
+				log.info("db1RowItemColumnCount ->{} db2RowItemColumnCount->{}",db1RowItemColumnCount,db2RowItemColumnCount );
+				
+				for (int j = 0; j < db1RowItemColumnCount; j++) {
+					DBColumn db1RowColRecord = null;
+					DBColumn db2RowColRecord = null;
+					try {
+						db1RowColRecord = db1RowRecord.getColumns().get(j);
+						db2RowColRecord = db2RowRecord.getColumns().get(j);
+						ColumnCompareResult compareResultItem = compareItem(db1RowColRecord, db2RowColRecord);
+						rowCompareResult.getColumns().add(compareResultItem);
+					} catch (Exception e) {
+						log.error("Column not found Db2 IndexOutOfBoundsException -> {}",e.getMessage());
+					}
+				}
+				compareResult.add(rowCompareResult);
+				
+				
+			} catch (IndexOutOfBoundsException e) {
+				log.error("Rows not found in Db2 IndexOutOfBoundsException -> {}",e.getMessage());
 			}
-			compareResult.add(rowCompareResult);
-			
 		}
 		
 		return compareResult;
