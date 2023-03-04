f1551783d32b8b2a3816347d41b87fbd12f1d266
aaklabs <connectakonline@gmail.com>
2023-03-04 16:56:24 -0500

    Match, MisMatch Logic Added

--- datadiff-be/src/main/java/com/aak/util/compare/service/ExcelHelper.java
+++ datadiff-be/src/main/java/com/aak/util/compare/service/ExcelHelper.java
@@ -42,7 +42,7 @@ public class ExcelHelper {
 		try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
 
 			generateExcelSheet(compareResponse, workbook, true, "Complete DataSet");
-			generateExcelSheet(compareResponse, workbook, false, "Diff DataSet");
+			generateExcelSheet(compareResponse, workbook, false, "Diff Row DataSet");
 
 			workbook.write(out);
 			return new ByteArrayInputStream(out.toByteArray());
@@ -56,48 +56,58 @@ public class ExcelHelper {
 		// Header start
 		addHeaderToWorksheet(completeDataSheet, compareResponse.getColumnNames());
 		// Header end
+		
 		int rowIdx = 1;
 		for (GridRow gridRowItem : compareResponse.getRows()) {
-
 			if(show_all_data) {
-				Row row = completeDataSheet.createRow(rowIdx++);
-				int colIdx = 0;
-				for (GridRowColumn colValue : gridRowItem.getRows()) {
-					final Cell excelCell = row.createCell(colIdx++);
-					if (colValue.isDifferent()) {
-						XSSFCellStyle badCellStyle = getBadCellStyle(completeDataSheet);
-						excelCell.setCellValue(colValue.getColumnValue().toString());
-						excelCell.setCellStyle(badCellStyle);
-					} else {
-						excelCell.setCellValue(colValue.getColumnValue().toString());
-					}
-				}
+				createExcelRow(completeDataSheet, rowIdx, gridRowItem);
 			}
 			else {
 				if(gridRowItem.isDifferent()) {
-					Row row = completeDataSheet.createRow(rowIdx++);
-					int colIdx = 0;
-					for (GridRowColumn colValue : gridRowItem.getRows()) {
-						final Cell excelCell = row.createCell(colIdx++);
-						if (colValue.isDifferent()) {
-							XSSFCellStyle badCellStyle = getBadCellStyle(completeDataSheet);
-							excelCell.setCellValue(colValue.getColumnValue().toString());
-							excelCell.setCellStyle(badCellStyle);
-						} else {
-							excelCell.setCellValue(colValue.getColumnValue().toString());
-						}
-					}	
+					createExcelRow(completeDataSheet, rowIdx, gridRowItem);	
 				}
 			}
-			
+			rowIdx++;
 		}
+	}
 
+	private void createExcelRow(Sheet completeDataSheet, int rowIdx, GridRow gridRowItem) throws DecoderException {
+		Row row = completeDataSheet.createRow(rowIdx);
+		
+		Cell statusColumnCell = row.createCell(0);
+		statusColumnCell.setCellValue(gridRowItem.isDifferent() ? "DIFFERENT": "SAME");
+		
+		if(gridRowItem.isDifferent()) {
+			statusColumnCell.setCellStyle(getBadRowCellStyle(completeDataSheet));
+		}
+		
+		int colIdx = 1;
+		for (GridRowColumn colValue : gridRowItem.getRows()) {
+			final Cell excelCell = row.createCell(colIdx++);
+			if (colValue.isDifferent()) {
+				XSSFCellStyle badCellStyle = getBadCellStyle(completeDataSheet);
+				excelCell.setCellValue(colValue.getColumnValue().toString());
+				excelCell.setCellStyle(badCellStyle);
+			} else {
+				excelCell.setCellValue(colValue.getColumnValue().toString());
+				
+				if(gridRowItem.isDifferent()) {
+					//row highlight for bad records
+					excelCell.setCellStyle(getBadRowCellStyle(completeDataSheet));
+				}
+				
+			}
+		}
 	}
 
 	private void addHeaderToWorksheet(Sheet completeDataSheet, Set<String> columnNames) {
 		log.info("Generate Headers");
 		final Row headerRow = completeDataSheet.createRow(0);
-		int cellCounter = 0;
+		
+		Cell statusHeaderColumnCell = headerRow.createCell(0);
+		statusHeaderColumnCell.setCellValue("STATUS");
+		
+		int cellCounter = 1;
 		for (String columnName : columnNames) {
 			Cell cell = headerRow.createCell(cellCounter);
 			cell.setCellValue(columnName);
@@ -111,6 +121,52 @@ public class ExcelHelper {
 		byte[] rgbBadFont = Hex.decodeHex(rgbBadFontColor); // get byte array from hex string
 
 		String rgbBadColor = "FFC7CE";
+		byte[] rgbB = Hex.decodeHex(rgbBadColor); // get byte array from hex string
+		XSSFColor badColor = new XSSFColor(rgbB, null); // IndexedColorMap has no usage until now. So it can be set
+														// null.
+
+		XSSFCellStyle cellStyle = (XSSFCellStyle) completeDataSheet.getWorkbook().createCellStyle();
+		cellStyle.setFillForegroundColor(badColor);
+		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		Font font = completeDataSheet.getWorkbook().createFont();
+		XSSFFont xssfFont = (XSSFFont) font;
+		xssfFont.setColor(new XSSFColor(rgbBadFont, null));
+		font.setBold(false);
+
+		cellStyle.setFont(font);
+
+		CellStyle headerCellStyle = completeDataSheet.getWorkbook().createCellStyle();
+		// Font headerFont = workbook.createFont();
+		// headerFont.setColor(IndexedColors.RED.index);
+
+		headerCellStyle.setFont(font);
+		cellStyle.setFont(font);
+		cellStyle.setBorderBottom(BorderStyle.THIN);
+		cellStyle.setBorderLeft(BorderStyle.THIN);
+		cellStyle.setBorderRight(BorderStyle.THIN);
+		cellStyle.setBorderTop(BorderStyle.THIN);
+
+		return cellStyle;
+	}
+	
+	
+	private XSSFCellStyle getBadRowCellStyle(Sheet completeDataSheet) throws DecoderException {
+		// bad red background
+		// red color
+		//String rgbBadFontColor = "9C0006";
+		
+		String rgbBadFontColor = "000000";
+		byte[] rgbBadFont = Hex.decodeHex(rgbBadFontColor); // get byte array from hex string
+
+		//String rgbBadColor = "F47b91";
+		// light pink
+		//String rgbBadColor = "F8a5b4";
+		
+		// orange
+		String rgbBadColor = "fbd6a2";
+		
+		
 		byte[] rgbB = Hex.decodeHex(rgbBadColor); // get byte array from hex string
 		XSSFColor badColor = new XSSFColor(rgbB, null); // IndexedColorMap has no usage until now. So it can be set
 														// null.
