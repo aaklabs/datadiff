e27bac0d594d24900282edb030673270eae57833
aaklabs <connectakonline@gmail.com>
2023-03-07 15:22:37 -0500

    Excel file import changes added.

---
+++ datadiff-be/src/main/java/com/aak/util/compare/diff/model/ExcelExportCellAlignment.java
@@ -0,0 +1,13 @@
+package com.aak.util.compare.diff.model;
+
+public enum ExcelExportCellAlignment {
+	LEFT("LEFT"),
+	CENTER("CENTER"),
+	RIGHT("RIGHT");
+	
+	private String label;
+	
+	ExcelExportCellAlignment(String label){
+		this.label = label;
+	}
+}
--- datadiff-be/src/main/java/com/aak/util/compare/service/ExcelHelper.java
+++ datadiff-be/src/main/java/com/aak/util/compare/service/ExcelHelper.java
@@ -12,6 +12,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -22,6 +23,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.aak.util.compare.diff.model.ExcelExportCellAlignment;
 import com.aak.util.compare.model.api.CompareResponse;
 import com.aak.util.compare.model.api.GridRow;
 import com.aak.util.compare.model.api.GridRowColumn;
@@ -40,10 +42,8 @@ public class ExcelHelper {
 	public ByteArrayInputStream toExcelInputStream(CompareResponse compareResponse) throws DecoderException {
 
 		try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
-
 			generateExcelSheet(compareResponse, workbook, true, "All rows");
 			generateExcelSheet(compareResponse, workbook, false, "Mismatched");
-
 			workbook.write(out);
 			return new ByteArrayInputStream(out.toByteArray());
 		} catch (IOException e) {
@@ -60,10 +60,12 @@ public class ExcelHelper {
 		int rowIdx = 1;
 		for (GridRow gridRowItem : compareResponse.getRows()) {
 			if(show_all_data) {
+				// all rows
 				createExcelRow(completeDataSheet, rowIdx, gridRowItem);
 			}
 			else {
 				if(gridRowItem.isDifferent()) {
+					// show rows which has difference in columns
 					createExcelRow(completeDataSheet, rowIdx, gridRowItem);	
 				}
 			}
@@ -78,44 +80,58 @@ public class ExcelHelper {
 		statusColumnCell.setCellValue(gridRowItem.isDifferent() ? "MISMATCH": "MATCH");
 		
 		if(gridRowItem.isDifferent()) {
-			statusColumnCell.setCellStyle(getBadRowCellStyle(completeDataSheet));
+			statusColumnCell.setCellStyle(getBadRowRegularCellStyle(completeDataSheet, ExcelExportCellAlignment.CENTER));
+		}
+		else {
+			statusColumnCell.setCellStyle(getMatchedCellStyle(completeDataSheet, ExcelExportCellAlignment.CENTER));
 		}
 		
 		int colIdx = 1;
 		for (GridRowColumn colValue : gridRowItem.getRows()) {
 			final Cell excelCell = row.createCell(colIdx++);
 			if (colValue.isDifferent()) {
-				XSSFCellStyle badCellStyle = getBadCellStyle(completeDataSheet);
+				XSSFCellStyle badCellStyle = getBadCellStyle(completeDataSheet, colValue.getColumnName().contains("_db1") ? ExcelExportCellAlignment.RIGHT : ExcelExportCellAlignment.LEFT);
 				excelCell.setCellValue(colValue.getColumnValue().toString());
 				excelCell.setCellStyle(badCellStyle);
 			} else {
 				excelCell.setCellValue(colValue.getColumnValue().toString());
-				
 				if(gridRowItem.isDifferent()) {
 					//row highlight for bad records
-					excelCell.setCellStyle(getBadRowCellStyle(completeDataSheet));
+					excelCell.setCellStyle(getBadRowRegularCellStyle(completeDataSheet, colValue.getColumnName().contains("_db1") ? ExcelExportCellAlignment.RIGHT : ExcelExportCellAlignment.LEFT));
+				}
+				else {
+					excelCell.setCellStyle(getMatchedCellStyle(completeDataSheet, colValue.getColumnName().contains("_db1") ? ExcelExportCellAlignment.RIGHT : ExcelExportCellAlignment.LEFT));
 				}
 				
 			}
 		}
 	}
 
-	private void addHeaderToWorksheet(Sheet completeDataSheet, Set<String> columnNames) {
+	private void addHeaderToWorksheet(Sheet completeDataSheet, Set<String> columnNames) throws DecoderException {
 		log.info("Generate Headers");
 		final Row headerRow = completeDataSheet.createRow(0);
 		
 		Cell statusHeaderColumnCell = headerRow.createCell(0);
 		statusHeaderColumnCell.setCellValue("STATUS");
+		statusHeaderColumnCell.setCellStyle(getMatchedCellStyle(completeDataSheet, ExcelExportCellAlignment.CENTER));
 		
 		int cellCounter = 1;
 		for (String columnName : columnNames) {
-			Cell cell = headerRow.createCell(cellCounter);
-			cell.setCellValue(columnName);
+			Cell headerCell = headerRow.createCell(cellCounter);
+			headerCell.setCellValue(columnName);
+			
+			if(columnName.contains("_db1")) {
+				headerCell.setCellStyle(getHeaderCellStyle(completeDataSheet, ExcelExportCellAlignment.CENTER, "FFC000"));
+			}
+			else {
+				headerCell.setCellStyle(getHeaderCellStyle(completeDataSheet, ExcelExportCellAlignment.CENTER, "00B0F0"));
+			}
+			
 			cellCounter++;
 		}
 	}
 
-	private XSSFCellStyle getBadCellStyle(Sheet completeDataSheet) throws DecoderException {
+	private XSSFCellStyle getBadCellStyle(Sheet completeDataSheet, ExcelExportCellAlignment excelExportCellAlignment) throws DecoderException {
 		// bad red background
 		String rgbBadFontColor = "9C0006";
 		byte[] rgbBadFont = Hex.decodeHex(rgbBadFontColor); // get byte array from hex string
@@ -142,16 +158,15 @@ public class ExcelHelper {
 
 		headerCellStyle.setFont(font);
 		cellStyle.setFont(font);
-		cellStyle.setBorderBottom(BorderStyle.THIN);
-		cellStyle.setBorderLeft(BorderStyle.THIN);
-		cellStyle.setBorderRight(BorderStyle.THIN);
-		cellStyle.setBorderTop(BorderStyle.THIN);
+		
+		setStandardExcelCellStyles(excelExportCellAlignment, cellStyle);
+		
 
 		return cellStyle;
 	}
 	
 	
-	private XSSFCellStyle getBadRowCellStyle(Sheet completeDataSheet) throws DecoderException {
+	private XSSFCellStyle getBadRowRegularCellStyle(Sheet completeDataSheet,  ExcelExportCellAlignment excelExportCellAlignment) throws DecoderException {
 		// bad red background
 		// red color
 		//String rgbBadFontColor = "9C0006";
@@ -163,8 +178,8 @@ public class ExcelHelper {
 		// light pink
 		//String rgbBadColor = "F8a5b4";
 		
-		// orange
-		String rgbBadColor = "fbd6a2";
+		// blue
+		String rgbBadColor = "9BC2E6";
 		
 		
 		byte[] rgbB = Hex.decodeHex(rgbBadColor); // get byte array from hex string
@@ -188,12 +203,100 @@ public class ExcelHelper {
 
 		headerCellStyle.setFont(font);
 		cellStyle.setFont(font);
-		cellStyle.setBorderBottom(BorderStyle.THIN);
-		cellStyle.setBorderLeft(BorderStyle.THIN);
-		cellStyle.setBorderRight(BorderStyle.THIN);
-		cellStyle.setBorderTop(BorderStyle.THIN);
+		
+		
+		setStandardExcelCellStyles(excelExportCellAlignment, cellStyle);
 
 		return cellStyle;
 	}
 
+	
+	private XSSFCellStyle getMatchedCellStyle(Sheet completeDataSheet,  ExcelExportCellAlignment excelExportCellAlignment) throws DecoderException {
+		
+		XSSFCellStyle cellStyle = (XSSFCellStyle) completeDataSheet.getWorkbook().createCellStyle();
+		
+		//black font color
+		String rgbBadFontColor = "000000";
+		byte[] rgbBadFont = Hex.decodeHex(rgbBadFontColor); // get byte array from hex string
+
+		Font font = completeDataSheet.getWorkbook().createFont();
+		XSSFFont xssfFont = (XSSFFont) font;
+		xssfFont.setColor(new XSSFColor(rgbBadFont, null));
+		font.setBold(false);
+
+		cellStyle.setFont(font);
+
+		CellStyle headerCellStyle = completeDataSheet.getWorkbook().createCellStyle();
+		headerCellStyle.setFont(font);
+		
+		cellStyle.setFont(font);
+		
+		
+		setStandardExcelCellStyles(excelExportCellAlignment, cellStyle);
+
+		return cellStyle;
+	}
+	
+	private XSSFCellStyle getHeaderCellStyle(Sheet completeDataSheet,  ExcelExportCellAlignment excelExportCellAlignment, String backgroundColor) throws DecoderException {
+		
+		// bad red background
+				// red color
+				//String rgbBadFontColor = "9C0006";
+				
+				String rgbBadFontColor = "000000";
+				byte[] rgbBadFont = Hex.decodeHex(rgbBadFontColor); // get byte array from hex string
+
+				//String rgbBadColor = "F47b91";
+				// light pink
+				//String rgbBadColor = "F8a5b4";
+				
+				// blue
+				String rgbBadColor = backgroundColor;
+				
+				
+				byte[] rgbB = Hex.decodeHex(rgbBadColor); // get byte array from hex string
+				XSSFColor badColor = new XSSFColor(rgbB, null); // IndexedColorMap has no usage until now. So it can be set
+																// null.
+
+				XSSFCellStyle cellStyle = (XSSFCellStyle) completeDataSheet.getWorkbook().createCellStyle();
+				cellStyle.setFillForegroundColor(badColor);
+				cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+				Font font = completeDataSheet.getWorkbook().createFont();
+				XSSFFont xssfFont = (XSSFFont) font;
+				xssfFont.setColor(new XSSFColor(rgbBadFont, null));
+				font.setBold(false);
+
+				cellStyle.setFont(font);
+
+				CellStyle headerCellStyle = completeDataSheet.getWorkbook().createCellStyle();
+				// Font headerFont = workbook.createFont();
+				// headerFont.setColor(IndexedColors.RED.index);
+
+				headerCellStyle.setFont(font);
+				cellStyle.setFont(font);
+				
+				
+				setStandardExcelCellStyles(excelExportCellAlignment, cellStyle);
+
+				return cellStyle;
+	}
+
+	private void setStandardExcelCellStyles(ExcelExportCellAlignment excelExportCellAlignment, XSSFCellStyle cellStyle) {
+		
+		cellStyle.setBorderBottom(BorderStyle.THIN);
+		cellStyle.setBorderLeft(BorderStyle.THIN);
+		cellStyle.setBorderRight(BorderStyle.THIN);
+		cellStyle.setBorderTop(BorderStyle.THIN);
+		
+		if(excelExportCellAlignment.equals(ExcelExportCellAlignment.RIGHT)) {
+			cellStyle.setAlignment(HorizontalAlignment.RIGHT);
+		}
+		else if(excelExportCellAlignment.equals(ExcelExportCellAlignment.LEFT)) {
+			cellStyle.setAlignment(HorizontalAlignment.LEFT);
+		}
+		else {
+			cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		}
+	}
 }
